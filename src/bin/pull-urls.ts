d9fd0cec4b9a035d88b4654453a463c051b40bd5
Lencse <leventeloki@gmail.com>
2023-04-06 00:29:47 +0200

    Fixed typo

--- src/bin/pull-urls.ts
+++ src/bin/pull-urls.ts
@@ -2,7 +2,7 @@ import { getDataFromSheet } from '../google/sheets'
 import { writeFileSync } from 'fs'
 
 export const pullUrls = async () => {
-    const rows = await getDataFromSheet('')
+    const rows = await getDataFromSheet()
     const urls = rows.slice(1).map((row) => ({
         shortUrl: row[0],
         link: row[1],
