cce03a4a3a60dcec66079cbdf0c04e29595bc13b
Lencse <leventeloki@gmail.com>
2023-04-09 03:20:53 +0200

    QR code page

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,13 @@
-import { getenv } from '~/config/getenv'
+import { strict as assert } from 'assert'
+
+assert(
+    typeof process.env['NEXT_PUBLIC_SITE_URL'] === 'string',
+    `NEXT_PUBLIC_SITE_URL env var not set`
+)
 
 export const config = {
-    siteUrl: getenv('SITE_URL'),
+    siteUrl: process.env['NEXT_PUBLIC_SITE_URL'],
+    // siteUrl: getenv('NEXT_PUBLIC_SITE_URL'),
     qrCodeSizeInPixels: 1600,
     qrCodeMargin: 2,
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,300;0,700;1,300;1,700&display=swap"
                     rel="stylesheet"
